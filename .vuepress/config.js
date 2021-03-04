59f20e183ed3f62d0409829ce701a4dc58ee8d1d
shaimole <57011853+shaimole@users.noreply.github.com>
2021-03-04 21:15:16 +0100

    added skils and stress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,10 +8,6 @@ module.exports = {
     ],
     sidebar :[
       {
-        title: 'pg',   // required
-        path: '/playground/',      // optional, link of the title, which should be an absolute path and must exist
-      },  
-      {
         title: 'Über Pen & Pendulum',   // required
         path: '/about/',      // optional, link of the title, which should be an absolute path and must exist
         collapsable: false, // optional, defaults to true
@@ -41,7 +37,35 @@ module.exports = {
         children: [ 
           ['/creation/classes/','Berufsklassen']
         ]
-      }
+      },
+      {
+        title: 'Aktionen',   // required
+        path: '/aktionen/',      // optional, link of the title, which should be an absolute path and must exist
+        sidebarDepth: 1    // optional, defaults to 1
+      },    
+      {
+        title: 'Eigenschaften',   // required
+        path: '/eigenschaften/',      // optional, link of the title, which should be an absolute path and must exist
+        sidebarDepth: 1    // optional, defaults to 1
+      },    
+      {
+        title: 'Kunden',   // required
+        path: '/kunden/',      // optional, link of the title, which should be an absolute path and must exist
+        sidebarDepth: 1    // optional, defaults to 1
+      },  
+      {
+        title: 'Stress',   // required
+        path: '/stress/',      // optional, link of the title, which should be an absolute path and must exist
+        sidebarDepth: 2,    // optional, defaults to 1
+      }, 
+      {
+        title: 'Fähigkeiten',   // required
+        path: '/skills/',      // optional, link of the title, which should be an absolute path and must exist
+        sidebarDepth: 2,    // optional, defaults to 1
+        children: [
+          ['/skills/handwerk/','Handwerk']
+        ]
+      } 
     ]
   }
 }
