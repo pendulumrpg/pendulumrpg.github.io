3fd7fb45da39192ee5f990b665a46d7f890292a7
shaimole <57011853+shaimole@users.noreply.github.com>
2021-08-01 16:43:20 +0200

    pwa first steps, and priest skills

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,24 @@
 module.exports = {
+  plugins: [
+    '@vuepress/pwa',
+    {
+      serviceWorker: true,
+      updatePopup: true
+    }
+    ],
   title: '',
+  head: [
+    [ "link",{ rel: "stylesheet",href: "/css/styles.css"}],
+    ['link', { rel: 'icon', href: 'images/Pendulum-Logo.svg' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: 'images/Pendulum-Logo.svg' }],
+    ['link', { rel: 'mask-icon', href: 'images/Pendulum-Logo.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: 'images/Pendulum-Logo.svg' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   themeConfig: {
     logo: '/images/Pendulum-Logo.svg',
     nav: [
@@ -65,7 +84,8 @@ module.exports = {
         children: [
             ['/skills/handwerk/','Handwerk'],
             ['/skills/kompetenzen/','Kompetenzen'],
-            ['/skills/magie/','Magie']
+            ['/skills/magie/','Magie'],
+            ['/skills/priest/','Priester']
         ]
       } 
     ]
